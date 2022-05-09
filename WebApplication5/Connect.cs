c6a4e443f4148d0731ba07f3eb2300d95ea25b40
Данил <@DESKTOP-NH71MJS>
2022-05-09 12:37:32 +0300

    Добавьте файлы проекта.

---
+++ WebApplication5/Connect.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace WebApplication5
+{
+    public class Connection
+    {
+        private static SqlConnection connection;
+        private Connection() { }
+        public static SqlConnection GetConnection()
+        {
+            if (connection == null)
+            {
+                connection = new SqlConnection("Data Source=SQL8001.site4now.net;Initial Catalog=db_a84e8b_bajen;User Id=db_a84e8b_bajen_admin;Password=Savelstan123");
+                connection.Open();
+            }
+            return connection;
+        }
+    }
+}
---
+++ WebApplication5/Controllers/RegController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MimeKit;
+using MailKit.Net.Smtp;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegisterController : ControllerBase
+    {
+        SqlOperations sqlOperations = new SqlOperations();
+        string msg = string.Empty;
+        private readonly ILogger<RegisterController> _logger;
+
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(string login, string email, string password)
+        {
+            if (sqlOperations.isLoginExist(login))
+            {
+                return "Login exist!";
+            }
+            else if (sqlOperations.isEmailExist(email))
+            {
+                return "Email exist!";
+            }
+            try
+            {
+                SendPassword(email, password);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return sqlOperations.Register(login, email, password);
+        }
+
+        void SendPassword(string email, string password)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "tanksinass@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("Hello", email));
+            emailMessage.Subject = "тема";
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = password
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.ConnectAsync("smtp.gmail.com", 465, false);
+                client.AuthenticateAsync("tanksinass@gmail.com", "Kanadic1488");
+                client.SendAsync(emailMessage);
+
+                client.DisconnectAsync(true);
+            }
+        }
+    }
+}
